25baf6f0172681d46801dad6205085236b2970f6
Gosia Greszczuk <gosia.greszczuk@gmail.com>
2023-07-16 08:48:24 +0000

    model box i display cwiczenia

---
+++ src/App/Exercises/HtmlCss/BoxModel/router-data.jsx
@@ -0,0 +1,19 @@
+import { BoxModel } from './index';
+
+export const blockRouterMetaData = {
+  path: 'box-and-display',
+  date: '13-07-2023',
+  linkLabel: 'BoxModel oraz display route',
+  blockNo: 1,
+  element: <BoxModel />,
+  tags: [
+    'css',
+    'box-model',
+    'display',
+    'paddding',
+    'margin',
+    'border',
+    'box-sizing',
+    'overflow',
+  ],
+};
