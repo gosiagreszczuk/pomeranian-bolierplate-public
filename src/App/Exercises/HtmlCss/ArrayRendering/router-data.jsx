71b7fdd45fa8eeaf8b62bad25cba289796c78803
Gosia Greszczuk <gosia.greszczuk@gmail.com>
2023-07-21 14:02:43 +0000

    poczatek array rendering

---
+++ src/App/Exercises/HtmlCss/ArrayRendering/router-data.jsx
@@ -0,0 +1,10 @@
+import { ArrayRendering } from './index';
+
+export const blockRouterMetaData = {
+  path: 'array-rendering',
+  date: '16-07-2023',
+  linkLabel: 'Renderowanie tablicy w reakcie',
+  blockNo: 14,
+  element: <ArrayRendering />,
+  tags: ['array', 'HTML', 'JavaScript', 'render', 'react', 'string', 'object'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -14,6 +14,7 @@ import { blockRouterMetaData as MediaFiles } from './MediaFiles/router-data';
 import { blockRouterMetaData as BoxModel } from './BoxModel/router-data';
 import { blockRouterMetaData as HTMLTables } from './HTMLTables/router-data';
 import { blockRouterMetaData as TablesTennis } from './TablesTennis/router-data';
+import { blockRouterMetaData as ArrayRendering } from './ArrayRendering/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
@@ -30,5 +31,6 @@ export const blockRouterMetaData = [
   BoxModel,
   HTMLTables,
   TablesTennis,
+  ArrayRendering,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
